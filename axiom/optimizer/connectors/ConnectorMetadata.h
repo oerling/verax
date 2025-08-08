14ec61989040a64466814ba4923bad1c2cc41b14
Orri Erling <oerling@fb.com>
2025-08-08 13:22:51 -0700

    xx

--- axiom/optimizer/connectors/ConnectorMetadata.h
+++ axiom/optimizer/connectors/ConnectorMetadata.h
@@ -509,17 +509,17 @@ struct LookupKeys {
   bool isAscending{true};
 };
 
-/// Describes how to shuffle data before a TableWriter.
+/// Describes how to repartition data before a TableWriter.
 struct WritePartitioning {
   /// Columns for partitioning,. Names refer to the column names in the insert
   /// table handle. Empty if any worker can write any row.
-  std::vector<std::string> columns;
+  const std::vector<std::string> columns;
 
   /// Specifies the partition function. nullptr if 'columns' is empty.
-  std::shared_ptr<const core::PartitionFunctionSpec> partitionSpec;
+  const std::shared_ptr<const core::PartitionFunctionSpec> partitionSpec;
 
   /// Maximum number of workers. For example, having more workers than there are
-  /// buckets makes no sense.
+  /// partitions makes no sense.
   const int32_t maxWorkers;
 };
 
@@ -527,7 +527,7 @@ struct WritePartitioning {
 /// example encapsulate a transaction state. The minimal
 /// implementation does nothing, which amounts to all write
 /// operations being non-isolated and autocommitting. Connector
-/// specific implementations have specify transaction functions.
+/// specific implementations have their specific transaction functions.
 class ConnectorSession {
   virtual ~ConnectorSession() = default;
 };
@@ -536,7 +536,24 @@ using ConnectorSessionPtr = std::shared_ptr<ConnectorSession>;
 
 /// Specifies what type of write is intended when initiating or concluding a
 /// write operation.
-enum class WriteKind { kInsert, kDelete, kUpdate };
+enum class WriteKind {
+  // Rows are added and all columns must be specified for the TableWriter. This
+  // covers insert, create table and replacing a Hive partition and any other
+  // use that adds whole rows.
+  kInsert,
+
+  // Individual rows are deleted. Only row ids as per
+  // ConnectorMetadata::rowIdHandles() are passed to the TableWriter.
+  kDelete,
+
+  // Column values in individual rows are changed. The TableWriter
+  // gets first the row ids as per ConnectorMetadata::rowIdHandles()
+  // and then new values for the columns being changed. The new values
+  // may overlap with row ids if the row id is a set of primary key
+  // columns.
+
+  kUpdate
+};
 
 class ConnectorMetadata {
  public:
@@ -602,6 +619,36 @@ class ConnectorMetadata {
     VELOX_UNSUPPORTED();
   }
 
+  /// Creates a table. 'tableName' is a name with optional 'schema.'
+  /// followed by table name. The connector gives the first part of
+  /// the three part name. The table properties are in 'options'. All
+  /// options must be understood by the connector. To create a table,
+  /// first make a ConnectorSession in a connector dependent manner,
+  /// then call createTable, then access the created layout(s) and
+  /// make an insert table handle for writing each. Insert data into
+  /// each layout and then call finishWrite on each. Normally a table
+  /// has one layout but if many exist, as in secondary indices or
+  /// materializations that are not transparently handled by an
+  /// outside system, the optimizer is expected to make plans that
+  /// write to all. In such cases the plan typically has a different
+  /// table writer for each materialization. Any transaction semantics
+  /// are connector dependent. Throws an error if the table exists,
+  /// unless 'deleteIfExists' is true, in which case the table is
+  /// silently deleted.  finishWrite should be called for all insert
+  /// table handles to complete the write also if no data is added. To
+  /// create an empty table, call createTable and then commit if the
+  /// connector is transactional. to create the table with data,
+  /// insert into all materializations, call finishWrite on each and
+  /// then commit the whole transaction if the connector requires
+  /// that.
+  void createTable(
+      const std::string& tableName,
+      const std::unordered_map<std::string, std::string>& options,
+      const ConnectorSessionPtr& session,
+      bool deleteIfExistts) {
+    VELOX_UNSUPPORTED();
+  }
+
   /// Creates an insert table handle for use with Velox TableWriter. '
   /// 'rowType' is the type of one row, including any partitioning or
   /// bucketing columns. The order may be significant, for example
@@ -619,37 +666,14 @@ class ConnectorMetadata {
   virtual ConnectorInsertTableHandlePtr createInsertTableHandle(
       const TableLayout& layout,
       const RowTypePtr& rowType,
-      std::unordered_map<std::string, std::string> options,
+      const std::unordered_map<std::string, std::string>& options,
       WriteKind kind,
       const ConnectorSessionPtr& session) {
     VELOX_UNSUPPORTED();
   }
 
-  /// Creaates a table. tableName' is a name with optional 'schema,.'
-  /// followed by table name. Theconnector gives the first part of the
-  /// three part name. The table properties are in 'options'. All
-  /// opttions must be understood by the connector. To create a table,
-  /// first call createTable, then access the created layout(s) and
-  /// make an insert table handle for writing each. Insert data into
-  /// each layout and then call finishWrite on each. Normally a table
-  /// has one layout but if many exist, as in secondary indices or
-  /// materializations that are not transparently handled by an
-  /// outside system, the optimizer is expected to make plans that
-  /// write to all.
-  /// Any transaction semanttics are connector
-  /// dependent. Throws an error if the table exists, unless
-  /// 'deleteIfExists' is true, in which case the table is silently deleted.
-  /// finishWrite should be called to complete the write also if no data is
-  /// added.
-  void createTable(
-      const std::string& tableName,
-      const std::unordered_map<std::string, std::string>& options,
-      const ConnectorSessionPtr& session,
-      bool deleteIfExistts) {
-    VELOX_UNSUPPORTED();
-  }
-
-  /// Returns specification for shuffling data before the table writer stage.
+  /// Returns specification for repartitioning data before the table writer
+  /// stage.
   virtual WritePartitioning writerShuffleInfo(
       const ConnectorInsertTableHandlePtr& handle) {
     VELOX_UNSUPPORTED();
@@ -657,7 +681,8 @@ class ConnectorMetadata {
 
   /// Finalizes a table write. This runs once after all the table writers have
   /// finished. The result sets from the table writer fragments are passed as
-  /// 'writerResults'. Their format and meaning is connector specific.
+  /// 'writerResults'. Their format and meaning is connector specific. the
+  /// RowType is given by the outputType() of the TableWriter.
   virtual void finishWrite(
       const ConnectorInsertTableHandlePtr& handle,
       const std::vector<VectorPtr>& writerResult,
