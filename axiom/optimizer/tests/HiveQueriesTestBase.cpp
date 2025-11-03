855a4945ab5443ff32750969e7b7d089b314cec3
Orri Erling <oerling@meta.com>
2025-11-02 20:19:49 -0800

    xx

--- axiom/optimizer/tests/HiveQueriesTestBase.cpp
+++ axiom/optimizer/tests/HiveQueriesTestBase.cpp
@@ -57,7 +57,7 @@ RowTypePtr HiveQueriesTestBase::getSchema(std::string_view tableName) {
       ->type();
 }
 
-void HiveQueriesTestBase::checkResults(
+  axiom::optimizer::PlanAndStats HiveQueriesTestBase::checkResults(
     std::string_view sql,
     const core::PlanNodePtr& referencePlan) {
   SCOPED_TRACE(sql);
@@ -65,11 +65,13 @@ void HiveQueriesTestBase::checkResults(
 
   auto statement = prestoParser_->parse(sql);
 
-  ASSERT_TRUE(statement->isSelect());
+  VELOX_CHECK(statement->isSelect());
   auto logicalPlan =
       statement->as<::axiom::sql::presto::SelectStatement>()->plan();
 
-  checkSame(logicalPlan, referencePlan);
+  auto plan = planVelox(logicalPlan, {.numWorkers = 1, .numDrivers = 1});
+  checkSame(plan, referencePlan);
+  return plan;
 }
 
 void HiveQueriesTestBase::checkResults(
--- axiom/optimizer/tests/HiveQueriesTestBase.h
+++ axiom/optimizer/tests/HiveQueriesTestBase.h
@@ -36,7 +36,7 @@ class HiveQueriesTestBase : public test::QueryTestBase {
   /// Returns a schema of a TPC-H table.
   velox::RowTypePtr getSchema(std::string_view tableName);
 
-  void checkResults(
+  axiom::optimizer::PlanAndStats checkResults(
       std::string_view sql,
       const velox::core::PlanNodePtr& referencePlan);
 
