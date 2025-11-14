d168217c3e1d3010eb82c295e70663d2edcc6b97
Orri Erling <oerling@meta.com>
2025-11-14 11:24:28 -0800

    xx

--- axiom/optimizer/tests/TpchPlanTest.cpp
+++ axiom/optimizer/tests/TpchPlanTest.cpp
@@ -280,11 +280,11 @@ class TpchPlanTest : public virtual test::HiveQueriesTestBase {
 
   std::unique_ptr<exec::test::TpchQueryBuilder> referenceBuilder_;
 
-#include "check_1.inc"
-#include "check_13.inc"
-#include "check_17.inc"
-#include "check_3.inc"
-#include "check_4.inc"
+#include "h01_check_1.inc"
+#include "h01_check_13.inc"
+#include "h01_check_17.inc"
+#include "h01_check_3.inc"
+#include "h01_check_4.inc"
 };
 
 TEST_F(TpchPlanTest, stats) {
@@ -312,7 +312,7 @@ TEST_F(TpchPlanTest, stats) {
 }
 
 TEST_F(TpchPlanTest, q01) {
-  defineCheckers1();
+  h01DefineCheckers1();
   auto logicalPlan =
       lp::PlanBuilder()
           .tableScan(exec::test::kHiveConnectorId, "lineitem")
@@ -342,7 +342,7 @@ TEST_F(TpchPlanTest, q02) {
 }
 
 TEST_F(TpchPlanTest, q03) {
-  defineCheckers3();
+  h01DefineCheckers3();
   lp::PlanBuilder::Context context{exec::test::kHiveConnectorId};
   auto logicalPlan =
       lp::PlanBuilder(context)
@@ -367,7 +367,7 @@ TEST_F(TpchPlanTest, q03) {
 }
 
 TEST_F(TpchPlanTest, q04) {
-  defineCheckers4();
+  h01DefineCheckers4();
   checkTpchSql(4);
 }
 
@@ -628,7 +628,7 @@ TEST_F(TpchPlanTest, q12) {
 }
 
 TEST_F(TpchPlanTest, q13) {
-  defineCheckers13();
+  h01DefineCheckers13();
   lp::PlanBuilder::Context context{exec::test::kHiveConnectorId};
   auto logicalPlan =
       lp::PlanBuilder(context)
@@ -678,7 +678,7 @@ TEST_F(TpchPlanTest, q16) {
 }
 
 TEST_F(TpchPlanTest, q17) {
-  defineCheckers17();
+  h01DefineCheckers17();
   checkTpchSql(17);
 }
 
